bb40bfca46f05bcc34777c7e81a6821fdc9da0ac
Irwin <irwinchan@me.com>
2015-10-17 20:19:30 -0400

    Add highlight winning squares function

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
 $(document).ready(function() {
 
-
-  function Player (number, marker) {
+  function Player (number) {
     this.moves = [];
     this.number = number;
-    this.marker = marker;
 
     this.move = function(square) {
       square.addClass("player" + this.number);
       square.removeClass("empty");
-      square.html(this.marker);
     }
   }
 
-  var winConditions = [
-    ["1-1", "1-2", "1-3"],
-    ["2-1", "2-2", "2-3"],
-    ["3-1", "3-2", "3-3"],
-    ["1-1", "2-1", "3-1"],
-    ["1-2", "2-2", "3-2"],
-    ["1-3", "2-3", "3-3"],
-    ["1-1", "2-2", "3-3"],
-    ["1-3", "2-2", "3-1"]
-  ];
+  function setGame(){
+    var squares = $(".grid-square")
+    for(i = 0; i < squares.length; i++){
+      $(squares[i]).removeClass("player1 player2 highlight");
+      if(!$(squares[i]).hasClass("empty")){
+        $(squares[i]).addClass("empty");
+      }
+    }
 
-  checkWin = function (player){
+    moveCounter = 0;
+    activePlayer = player1;
+
+    $(".board").on("click", ".grid-square", function(){
+      if($(this).hasClass("empty")) {
+        activePlayer.move($(this));
+        moveCounter++;
+
+        if(checkWin(activePlayer) == true) {
+          console.log("win");
+          $(".board").off();
+        }else if(moveCounter == 9) {
+          alert("Cats game no one wins!");
+        }
+
+        if (activePlayer == player1) {
+          activePlayer = player2;
+        }else{
+          activePlayer = player1;
+        }
+      }
+    });
+  }
+
+  function checkWin (player) {
     var playerNumber = "player" + player.number;
 
-    win = false;
+    var winConditions = [
+      ["1-1", "1-2", "1-3"],
+      ["2-1", "2-2", "2-3"],
+      ["3-1", "3-2", "3-3"],
+      ["1-1", "2-1", "3-1"],
+      ["1-2", "2-2", "3-2"],
+      ["1-3", "2-3", "3-3"],
+      ["1-1", "2-2", "3-3"],
+      ["1-3", "2-2", "3-1"]
+    ];
+
+    var win = false;
+
     for(i = 0; i < winConditions.length; i++) {
       for(j = 0; j < winConditions[i].length; j++) {
         var square = $("#" + winConditions[i][j]);
@@ -40,32 +71,31 @@ $(document).ready(function() {
         }
 
         if(win == true && j == 2) {
+          highlightSquares(winConditions[i]);
           return true;
         }
       }
     }
+
     return false;
   }
 
-  $(".board").on("click", ".grid-square", function(){
-    if($(this).hasClass("empty")) {
-      activePlayer.move($(this));
-
-      if(checkWin(activePlayer) == true) {
-        alert("WIN");
-      }
-
-      if (activePlayer == player1) {
-        activePlayer = player2;
-      }else{
-        activePlayer = player1;
-      }
+  function highlightSquares(squares){
+    for(i = 0; i < squares.length; i++) {
+      $("#" + squares[i]).addClass("highlight");
     }
+  }
+
+  $(".main").on("click", ".button.reset", function(){
+    setGame();
   });
 
-  var player1 = new Player(1, "X");
-  var player2 = new Player(2, "O");
+  var moveCounter = 0;
+  var player1 = new Player(1);
+  var player2 = new Player(2);
   var activePlayer = player1;
+
+  setGame();
 });
 
 
